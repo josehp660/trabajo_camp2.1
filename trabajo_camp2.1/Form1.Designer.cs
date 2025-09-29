a2578e311cf4b0979369d9410cf36cb0b2b13488
josehp660 <115431293+josehp660@users.noreply.github.com>
2025-09-28 19:22:05 -0600

    Agregar archivos de proyecto.

---
+++ trabajo_camp2.1/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace trabajo_camp2._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmbCategoria = new System.Windows.Forms.ComboBox();
+            this.txtPromedio = new System.Windows.Forms.TextBox();
+            this.bntCalcular = new System.Windows.Forms.Button();
+            this.lblDescuento = new System.Windows.Forms.Label();
+            this.lblNuevaPension = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(227, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(266, 70);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Pención";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(188, 128);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Categoria ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(188, 159);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Promedio";
+            // 
+            // cmbCategoria
+            // 
+            this.cmbCategoria.FormattingEnabled = true;
+            this.cmbCategoria.Items.AddRange(new object[] {
+            "A",
+            "B",
+            "C",
+            "D"});
+            this.cmbCategoria.Location = new System.Drawing.Point(283, 120);
+            this.cmbCategoria.Name = "cmbCategoria";
+            this.cmbCategoria.Size = new System.Drawing.Size(121, 24);
+            this.cmbCategoria.TabIndex = 3;
+            // 
+            // txtPromedio
+            // 
+            this.txtPromedio.Location = new System.Drawing.Point(283, 159);
+            this.txtPromedio.Name = "txtPromedio";
+            this.txtPromedio.Size = new System.Drawing.Size(100, 22);
+            this.txtPromedio.TabIndex = 4;
+            // 
+            // bntCalcular
+            // 
+            this.bntCalcular.BackColor = System.Drawing.Color.DarkGray;
+            this.bntCalcular.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bntCalcular.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.bntCalcular.Location = new System.Drawing.Point(283, 202);
+            this.bntCalcular.Name = "bntCalcular";
+            this.bntCalcular.Size = new System.Drawing.Size(100, 34);
+            this.bntCalcular.TabIndex = 5;
+            this.bntCalcular.Text = "Calcular";
+            this.bntCalcular.UseVisualStyleBackColor = false;
+            this.bntCalcular.Click += new System.EventHandler(this.bntCalcular_Click);
+            // 
+            // lblDescuento
+            // 
+            this.lblDescuento.AutoSize = true;
+            this.lblDescuento.Location = new System.Drawing.Point(191, 260);
+            this.lblDescuento.Name = "lblDescuento";
+            this.lblDescuento.Size = new System.Drawing.Size(70, 16);
+            this.lblDescuento.TabIndex = 6;
+            this.lblDescuento.Text = "Descunto :";
+            // 
+            // lblNuevaPension
+            // 
+            this.lblNuevaPension.AutoSize = true;
+            this.lblNuevaPension.Location = new System.Drawing.Point(194, 296);
+            this.lblNuevaPension.Name = "lblNuevaPension";
+            this.lblNuevaPension.Size = new System.Drawing.Size(105, 16);
+            this.lblNuevaPension.TabIndex = 7;
+            this.lblNuevaPension.Text = "Nueva Pension :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblNuevaPension);
+            this.Controls.Add(this.lblDescuento);
+            this.Controls.Add(this.bntCalcular);
+            this.Controls.Add(this.txtPromedio);
+            this.Controls.Add(this.cmbCategoria);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cmbCategoria;
+        private System.Windows.Forms.TextBox txtPromedio;
+        private System.Windows.Forms.Button bntCalcular;
+        private System.Windows.Forms.Label lblDescuento;
+        private System.Windows.Forms.Label lblNuevaPension;
+    }
+}
+
---
+++ trabajo_camp2.1/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace trabajo_camp2._1
+{
+    public partial class Form1 : Form
+    {
+        private object rbDescuento;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bntCalcular_Click(object sender, EventArgs e)
+        {
+            decimal pension = 0;
+
+            // Asignar pensión base según categoría
+            switch (cmbCategoria.Text)
+            {
+                case "A": pension = 550; break;
+                case "B": pension = 500; break;
+                case "C": pension = 450; break;
+                case "D": pension = 400; break;
+                default:
+                    MessageBox.Show("Seleccione una categoría.");
+                    return;
+            }
+
+            // Validar y obtener promedio
+            if (!decimal.TryParse(txtPromedio.Text, out decimal promedio) || promedio < 0 || promedio > 20)
+            {
+                MessageBox.Show("Ingrese un promedio válido entre 0 y 20.");
+                return;
+            }
+
+
+            // Calcular descuento
+            decimal descuento = 0;
+            if (promedio >= 14 && promedio <= 15.99m) descuento = 0.10m;
+            else if (promedio >= 16 && promedio <= 17.99m) descuento = 0.15m;
+            else if (promedio >= 18 && promedio <= 20) descuento = 0.25m;
+
+
+            // Aplicar descuento 
+    
+            {
+                pension -= pension * descuento;
+            }
+
+
+            // Mostrar resultado del escuento
+
+            decimal montoDescuento = pension * descuento;
+            lblDescuento.Text = $"Descuento : S/. {montoDescuento:F2}";
+
+
+            // Mostrar resultado en Nueva Pensión
+            decimal pensionFinal = pension - (pension * descuento);
+            lblNuevaPension.Text = $"Pensión : S/. {pensionFinal:F2}";
+
+
+        }
+    }
+}
